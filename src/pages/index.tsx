cd7de7135e727481ee1cfa3900f881829f16ff5b
yudai ueno <yudai7676@yahoo.co.jp>
2023-05-03 20:45:10 +0900

    indexページの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,23 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Button from "@mui/material/Button";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import styles from "../styles/Home.module.css";
 
-// export default function Home() {
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>GovApp</title>
-//         <meta name="description" content="toppage of govapp" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <main className={styles.main}>
-//         <h1>Welcome to GovApp!</h1>
-//       </main>
-
-//     </div>
-//   )
-// }
-
-import type { NextPage } from 'next'
-import { Button } from '@mui/material';
-
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <>
-      <Button variant="contained">Hello World</Button>
-    </>
-  )
-}
+    <div className={styles.container}>
+      <Head>
+        <title>GovApp</title>
+        <meta name="description" content="toppage of govapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default Home
+      <main className={styles.main}>
+        <h1>Welcome to GovApp!</h1>
+        <Button href="ranking/QuestionNum">
+          Go to Ranking Page <ArrowForwardIcon />
+        </Button>
+      </main>
+    </div>
+  );
+}
